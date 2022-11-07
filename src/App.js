c7c107336f1303b13513054a18fd4784c1bdf71d
Jasiuu2605 <98971547+Jasiuu2605@users.noreply.github.com>
2022-11-07 19:37:21 +0100

    creating a react version of balloon aplication

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Animated from './Animated';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="balloon-animation">
+      <Animated
+        className="flying-cloud"
+        src="/chmura.png"
+        ratioX="0.25"
+        ratioY="0.5"
+      />
+      <Animated
+        className="flying-cloud"
+        src="/chmura.png"
+        ratioX="0.15"
+        ratioY="0.23"
+      />
+      <Animated
+        className="flying-cloud"
+        src="/chmura.png"
+        ratioX="0.4"
+        ratioY="0.75"
+      />
+      <Animated
+        className="flying-cloud"
+        src="/chmura.png"
+        ratioX="0.23"
+        ratioY="0.09"
+      />
+      <Animated
+        className="flying-cloud"
+        src="/chmura.png"
+        ratioX="0.56"
+        ratioY="0.67"
+      />
+      <Animated
+        className="flying-balloon"
+        src="/balloon.png"
+        ratioX="0.29"
+        ratioY="0.29"
+      />
     </div>
   );
 }
